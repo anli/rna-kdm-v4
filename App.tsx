eafb5814c8bae1369817dca2e859de7f8e74cf39
Su Anli <sual0001@gmail.com>
2020-07-29 16:46:38 +0800

    feat(app): add theme support for dark mode

--- App.tsx
+++ App.tsx
@@ -2,15 +2,20 @@ import Navigation from '@navigation';
 import {persisted, store} from '@store';
 import React from 'react';
 import 'react-native-gesture-handler';
-import {Provider as PaperProvider} from 'react-native-paper';
+import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
 import {Provider as StoreProvider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
+
+const theme = {
+  ...DefaultTheme,
+  dark: true,
+};
 
 const App = () => {
   return (
     <StoreProvider store={store}>
       <PersistGate loading={null} persistor={persisted.persistor}>
-        <PaperProvider>
+        <PaperProvider theme={theme}>
           <Navigation />
         </PaperProvider>
       </PersistGate>
